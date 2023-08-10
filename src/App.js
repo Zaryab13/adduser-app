c91ff778631ceaf7ce946d7dc78f467f018e3aa1
Zaryab Khan <93703454+Zaryab13@users.noreply.github.com>
2023-08-10 17:46:54 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Adduser from "./components/users/addUser/Adduser";
+import UserList from "./components/users/userList/UserList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsers) => {
+      return [
+        ...prevUsers,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <Adduser onAddUser={addUserHandler}></Adduser>
+      <UserList users={usersList}></UserList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/users/addUser/Adduser.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Button from "../../UI/Button";
+import Card from "../../UI/Card";
+import styles from "./Adduser.module.css";
+
+const AddUser = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    if (enteredName.trim().length === 0 || enteredAge.length === 0) {
+        return;
+    }
+    if (enteredAge < 1) {
+        return;
+    }
+    props.onAddUser(enteredName, enteredAge);
+
+
+    setEnteredName("");
+    setEnteredAge("");
+  };
+
+  const enteredNameHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const enteredAgeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={enteredNameHandler}
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+      
